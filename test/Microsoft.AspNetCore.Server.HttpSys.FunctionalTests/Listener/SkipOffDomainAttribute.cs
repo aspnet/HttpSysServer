84721bdca9b43d9334558e4a3296aa7ad71da56a
Chris R <Tratcher@outlook.com>
2017-01-17 12:45:05 -0800

    Remove net451, use only netstandard. Move samples and tests to net46.

--- test/Microsoft.AspNetCore.Server.HttpSys.FunctionalTests/Listener/SkipOffDomainAttribute.cs
+++ test/Microsoft.AspNetCore.Server.HttpSys.FunctionalTests/Listener/SkipOffDomainAttribute.cs
@@ -18,7 +18,7 @@ namespace Microsoft.AspNetCore.Server.HttpSys.Listener
             {
                 try
                 {
-#if NET451
+#if NET46
                     return !string.IsNullOrEmpty(System.DirectoryServices.ActiveDirectory.Domain.GetComputerDomain().Name);
 #endif
                 }
