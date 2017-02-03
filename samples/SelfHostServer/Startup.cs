ca0480f1912fde6bd65207b517c5576c2b070293
Chris R <Tratcher@outlook.com>
2017-02-03 11:53:23 -0800

    Queue attach demo

--- samples/SelfHostServer/Startup.cs
+++ samples/SelfHostServer/Startup.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Server.HttpSys;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -33,12 +35,40 @@ namespace SelfHostServer
 
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder().AddCommandLine(options =>
+            {
+                // -ap "queuename" -v "v4.0" -l "webengine4.dll" -a \\.\pipe\iisipm853a4108-f312-4cda-b9be-9267f950c97b -h "C:\inetpub\temp\apppools\DefaultAppPool\DefaultAppPool.config" -w "" -m 0 -t 20 -ta 0
+                options.Args = args;
+                options.SwitchMappings = new Dictionary<string, string>()
+                {
+                    { "-ap", "queuename" },
+                    { "-v", "version" },
+                    { "-l", "load" },
+                    { "-a", "pipe" },
+                    { "-h", "config" },
+                    { "-w", "window" },
+                    { "-m", "magic" },
+                    { "-t", "time" },
+                    { "-ta", "talent" },
+                };
+            }).Build();
+            /*
+            var host0 = new WebHostBuilder()
+                .UseStartup<Startup>()
+                .UseHttpSys(options =>
+                {
+                    options.AttachToExistingRequestQueue = false;
+                    options.RequestQueueName = config["queuename"];
+                })
+                .Build();
+            host0.Start();
+            */
             var host = new WebHostBuilder()
                 .UseStartup<Startup>()
                 .UseHttpSys(options =>
                 {
-                    options.Authentication.Schemes = AuthenticationSchemes.None;
-                    options.Authentication.AllowAnonymous = true;
+                    options.AttachToExistingRequestQueue = true;
+                    options.RequestQueueName = config["queuename"];
                 })
                 .Build();
 
